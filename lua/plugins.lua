6521d5d85e06b7bc06c43aff3c583da1ec47b25a
lcdse7en <2353442022@qq.com>
2022-11-05 02:24:40 +0800

    emoji

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -92,7 +92,7 @@ return require("packer").startup {
     use { "hrsh7th/cmp-cmdline", after = "cmp-path" }
     use { "hrsh7th/cmp-calc", after = "cmp-cmdline" }
     use { "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp", after = "cmp-calc" }
-    --use { "hrsh7th/cmp-emoji", config = "require('plugins.emoji')" }
+    use { "hrsh7th/cmp-emoji", config = "require('plugins.emoji')" }
     use {
       "David-Kunz/cmp-npm",
       after = "cmp-tabnine",
